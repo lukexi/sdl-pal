6675750548de540e62586f71a8286e27ba4ec089
Luke Iannini <lukexi@me.com>
2017-02-14 18:29:38 -0800

    Deps update. Animate demo

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,14 @@ module Main where
 
 import SDL.Pal
 import Graphics.GL
+import Data.Time
 
 main :: IO ()
 main = do
-	window <- createGLWindow "SDL-Pal"
+    window <- createGLWindow "SDL-Pal"
 
-	whileWindow window $ \events -> do
-		glClearColor 1 1 1 1
-		glClear GL_COLOR_BUFFER_BIT
-		glSwapWindow window
+    whileWindow window $ \events -> do
+        now <- (/2) . (+1) . sin . realToFrac . utctDayTime <$> getCurrentTime
+        glClearColor now 0.1 0.2 1
+        glClear GL_COLOR_BUFFER_BIT
+        glSwapWindow window
